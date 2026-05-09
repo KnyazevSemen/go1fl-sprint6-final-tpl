056abb259ae2d8df324f32070303d10cc3a19bda
KnyazevSemen <knyazevsemen@gmail.com>
2026-05-09 16:31:09 +0700

    fix run server2

--- cmd/main.go
+++ cmd/main.go
@@ -9,16 +9,11 @@ import (
 )
 
 func main() {
-
 	logger := log.New(os.Stdout, "SERVER: ", log.LstdFlags)
 
 	srv := server.New(logger)
 
-	// Запускаем сервер — это единственная операция после создания
-	// ListenAndServe() блокирует выполнение, поэтому сервер работает до прерывания
 	if err := srv.HttpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		logger.Fatalf("ошибка запуска сервера: %v", err)
 	}
-
-	logger.Println("Сервер успешно запущен")
 }
